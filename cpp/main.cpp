36602673ae72bacd44afdc51b65f8e5f960d0a0e
ahnnib <binhannguyenba@gmail.com>
2024-12-28 19:45:03 +0700

    24/12/28

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,1 +1,49 @@
-﻿
+﻿#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define dbg cout << "dfad\n";
+
+ll l, r;
+ll dp[18][180][5130];
+int w[10];
+ll f(vector<int> &dig, int vt, int t1, int t2, ll t3, bool tight) {
+    if (vt < 0) {
+        if (__gcd(t1, t2) == 1) return t3;
+        return 0;
+    }
+    if (tight != 1 && dp[vt][t1][t2] != -1) return dp[vt][t1][t2];
+    ll res = 0;
+    int lim = tight ? dig[vt] : 9;
+    for (int i = 0; i <= lim; i++) {
+        res += f(dig, vt - 1, t1 + i, t2 + w[i], t3 * 10 + i, tight & (dig[vt] == i));
+    }
+    if (!tight) dp[vt][t1][t2] = res;
+    return res;
+}
+vector<int> get(ll x) {
+    vector<int> res;
+    for(; x; x /= 10) {
+        res.push_back(x % 10);
+        x /= 10;
+    }
+    return res;
+}
+void sol() {
+    cin >> l >> r;
+    // for (ll i = 0; i <= 100; i++) {
+    //     vector<int> dig = get(i);
+    //     cout << f(dig, dig.size()-1, 0, 0, 1) << ' ';
+    // }
+    vector<int> dig_l = get(l-1), dig_r = get(r);
+    ll g2 = f(dig_r, dig_r.size()-1, 0, 0, 0, 1);
+    ll g1 = f(dig_l, dig_l.size()-1, 0, 0, 0, 1);
+    cout << g2 << ' ' << g1 << '\n';
+}
+
+int main() {
+    memset(dp, -1, sizeof dp);
+    for (int i = 1; i <= 9; i++) w[i] = w[i-1] + i*i;
+    int t = 1;
+    // cin >> t;
+    while(t--) sol();
+}
---
+++ java/abstraction/Student/src/Student/Student.java
@@ -0,0 +1,53 @@
+package Student;
+
+import java.util.Scanner;
+
+public abstract class Student {
+    private String masv = "-";
+    private String name = "-";
+    private int age = 0;
+
+    public Student() {}
+    public Student(String _masv, String _name, int _age) {
+        masv = _masv;
+        name = _name;
+        age = _age;
+    }
+
+    public String getMasv() {
+        return masv;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setMasv(String masv) {
+        this.masv = masv;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        // return "(" + masv + ", " + name + ", " + age + ")";
+        return String.format("(%s, %s, %d)", this.masv, this.name, this.age);
+    }
+
+    public void input() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("masv: ");
+        masv = sc.nextLine();
+        System.out.print("name: ");
+        name = sc.nextLine();
+        System.out.print("age: ");
+        age = sc.nextInt();
+        sc.close();
+    }
+    public abstract float calculateAverage();
+}
